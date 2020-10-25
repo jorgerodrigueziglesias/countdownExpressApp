065b47e26a127067af4b56c59590d1e703e85867
Jorge Rodriguez Iglesias <jorgerodrigueziglesias@gmail.com>
2020-10-25 20:45:50 +0100

    init

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+var launchTime = new Date("12/29/2021 10:00").getTime();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var currentTime = new Date().getTime();
+  var numberOfMilliseconds = parseInt(launchTime - currentTime);
+  res.render('index', { title: 'Countdown to Launch', countDown: numberOfMilliseconds });
+});
+
+module.exports = router;
